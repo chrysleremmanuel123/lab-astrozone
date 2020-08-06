f4f7f8e027addb1f96bf3b0e3ef90edfc98964f6
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-08-06 10:55:28 +0530

    astrozone-lab

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,20 @@
 package model;
 
+
 //Type your code here
+ public class User{
+	 
+	 String name;
+	 public int date;
+	 public int month;
+	 public User(String name, String date, String month) {
+		
+		 this.name=name;
+		 this.date=Integer.parseInt(date);
+		 this.month=Integer.parseInt(month);
+		 
+	}
+
+	
+
+ }
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,49 @@
 package service;
+import model.User;
 
 // Type your code
+
+public class AstroCalculator{
+	public String findSign(User user) {
+	int date=user.date;
+		int month=user.month;
+		if((date>21 && month == 12) ||  (date<=19 && month == 1)){
+			return "Capricon";
+		}
+		if((date>20 && month ==1) ||  (date<=18 && month == 2)){
+			return "Aquarius";
+		}
+		if(date>20 && month ==3 || (date<=21 && month == 4)) {
+			return "Aries";
+		}
+		if(date>20 && month ==5 || (date<=22 && month == 6)) {
+			return "Cancer";
+		}
+		if(date>20 && month ==4 || (date<=20 && month == 5)) {
+			return "Gemini";
+		}
+		if(date>22 && month ==7 || (date<=22 && month == 8)) {
+			return "Aries";
+		}
+		if(date>23 && month ==9 || (date<=22 && month == 10)) {
+			return "Libra";
+		}
+		if(date>19 && month ==2 || (date<=20 && month == 3)) {
+			return "Pisces";
+		}
+		if(date>23 && month ==11 || (date<=21 && month == 12)) {
+			return "Sagittarius";
+		}
+		if(date>23 && month ==10 || (date<=22 && month == 11)) {
+			return "Scorpio";
+		}
+		if(date>19 && month ==4 || (date<=20 && month == 5)) {
+			return "Taurus";
+		}
+		if(date>23 && month ==8 || (date<=22 && month == 9)) {
+			return "Virgo";
+		}
+		return "false";
+	
+	}
+}
